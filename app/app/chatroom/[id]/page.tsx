ec9f2cda8a267deb2a824af9bd6efe082504b993
Madhav-Gupta-28 <guptamadhav2828@gmail.com>
2024-08-27 16:04:54 +0530

    implement basic attestation

--- app/app/chatroom/[id]/page.tsx
+++ app/app/chatroom/[id]/page.tsx
@@ -4,7 +4,14 @@ import React, { useRef, useState } from 'react';
 import ChatroomWrapper from '../../../components/ChatroomWrapper';
 import { useParams } from 'next/navigation';
 import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton,} from '@chakra-ui/react';
-import { useDisclosure , Button , FormControl , FormLabel , Input } from '@chakra-ui/react'
+import { useDisclosure , Button , FormControl , FormLabel , Input } from '@chakra-ui/react';
+
+import {  SignProtocolClient,SpMode,  EvmChains,  delegateSignAttestation, delegateSignRevokeAttestation, delegateSignSchema,} from "@ethsign/sp-sdk";
+import { privateKeyToAccount } from "viem/accounts";
+const privateKey = `0x0e56dc56de5142b9f29bb8e448c0c6c332b352d7d17e8ac0bce8b25273c92896`; // private key
+
+import { useWallet } from '@/app/WalletContext';
+
 
 interface ChatroomPageProps{
   name: string;
@@ -13,17 +20,44 @@ interface ChatroomPageProps{
 
 const ChatroomPage: React.FC<ChatroomPageProps> = ({name,hashtag}) => {
 
+  const { address } = useWallet();
+
   const { isOpen, onOpen, onClose } = useDisclosure();
-
   const betInputRef = useRef<HTMLInputElement>(null);
-
 
   const params = useParams();
   const roomId = params.id as string;
 
-  const bet = () => {
+  // Sign Protocol Clint 
+  const client = new SignProtocolClient(SpMode.OnChain, {
+    chain: EvmChains.arbitrumSepolia,
+    account: privateKeyToAccount(privateKey), // Optional if you are using an injected provider
+  });
+  
+
+  const bet = async() => {
     const currentBetAmount = betInputRef.current?.value;
     console.log(currentBetAmount);
+    console.log(privateKey);
+
+    if(!address) {
+      alert("Wallet not connected");
+      return
+    }
+
+    console.log(address)
+
+    // ***** Some Thoughts before doing the attestions Their is one way.. like we can do the tx first and then pash the tx hash in attestions
+    // *** We can use the schema hooks to do the attestion and receive payments from the user based on their score.
+
+
+    const createAttestationRes = await client.createAttestation({
+      schemaId: "0xbf",
+      data: { user_id: address , meme_id : "memeName_1" , stake_amount : currentBetAmount , bet_timestamp : Date.now() , schema_hook_data : "......................... "  },
+      indexingValue: `${address.toLowerCase()}_${roomId}`,
+    });
+
+    console.log(createAttestationRes);
   }
 
   return (
