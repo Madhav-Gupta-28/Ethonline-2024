9fd0842072e2820818158d439a3fc5571dd141a8
Agrim <agrimsharma1714@gmail.com>
2024-08-31 10:09:06 +0530

    meme battle contract changes

--- contracts/src/MemeBattle.sol
+++ contracts/src/MemeBattle.sol
@@ -13,24 +13,48 @@ contract MemeBattle is ISPHook {
         bool hasStaked;
     }
 
-    enum Action { USER_BET, MEME_WIN}
+    enum Action {
+        USER_BET,
+        MEME_WIN
+    }
 
     string[] public memeNames;
     address[] public memeTokenAddresses;
     address public creator;
     mapping(address => mapping(address => Staker)) public stakers;
     mapping(address => uint256) public totalStakedPerMeme;
+    mapping(address => address[]) public stakersPerMeme;
     address public winningMemeToken;
     bool public battleEnded;
 
-    event Staked(address indexed user, address indexed memeToken, uint256 amount);
-    event Unstaked(address indexed user, address indexed memeToken, uint256 amount);
+    event Staked(
+        address indexed user,
+        address indexed memeToken,
+        uint256 amount
+    );
+    event Unstaked(
+        address indexed user,
+        address indexed memeToken,
+        uint256 amount
+    );
     event MemeBattleEnded(address indexed winningMemeToken);
-    event RewardDistributed(address indexed user, address indexed memeToken, uint256 reward);
+    event RewardDistributed(
+        address indexed user,
+        address indexed memeToken,
+        uint256 reward
+    );
     event MessageSent(address indexed user, string message);
-    event UserScoreUpdated(address user, uint256 scoreBefore, uint256 scoreAfter);
+    event UserScoreUpdated(
+        address user,
+        uint256 scoreBefore,
+        uint256 scoreAfter
+    );
 
-    constructor(string[] memory _memeNames, address _creator, address _userContractAddress) {
+    constructor(
+        string[] memory _memeNames,
+        address _creator,
+        address _userContractAddress
+    ) {
         memeNames = _memeNames;
         creator = _creator;
         userContract = User(_userContractAddress);
@@ -41,11 +65,14 @@ contract MemeBattle is ISPHook {
         address attester,
         uint64 schemaId,
         uint64 attestationId,
-        bytes calldata  extraData
-        ) external payable  {
+        bytes calldata extraData
+    ) external payable {
         require(!battleEnded, "Battle has ended");
 
-        (Action action, address memeToken, uint256 amount) = abi.decode(extraData, (Action, address, uint256));
+        (Action action, address memeToken, uint256 amount) = abi.decode(
+            extraData,
+            (Action, address, uint256)
+        );
         require(validMemeToken(memeToken), "Invalid meme token");
 
         if (action == Action.USER_BET) {
@@ -55,6 +82,7 @@ contract MemeBattle is ISPHook {
             staker.amount += amount;
             staker.hasStaked = true;
             totalStakedPerMeme[memeToken] += amount;
+            stakersPerMeme[memeToken].push(attester);
 
             emit Staked(attester, memeToken, amount);
 
@@ -64,9 +92,11 @@ contract MemeBattle is ISPHook {
             uint256 userScoreAfter = userContract.getUserScore(attester);
 
             emit UserScoreUpdated(attester, userScoreBefore, userScoreAfter);
-
         } else if (action == Action.MEME_WIN) {
-            require(attester == creator, "Only the creator can declare a winner");
+            require(
+                attester == creator,
+                "Only the creator can declare a winner"
+            );
             require(!battleEnded, "Battle already ended");
 
             winningMemeToken = memeToken;
@@ -88,36 +118,54 @@ contract MemeBattle is ISPHook {
                 address currentMemeToken = memeTokenAddresses[i];
                 bool isWinningMeme = (currentMemeToken == winningMemeToken);
 
-                for (uint256 j = 0; j < memeTokenAddresses.length; j++) {
-                    address staker = address(uint160(j)); // Replace with actual staker address
+                for (
+                    uint256 j = 0;
+                    j < stakersPerMeme[currentMemeToken].length;
+                    j++
+                ) {
+                    address staker = stakersPerMeme[currentMemeToken][j];
                     if (stakers[currentMemeToken][staker].amount > 0) {
-                        uint256 userStake = stakers[currentMemeToken][staker].amount;
-                        uint256 userScoreBefore = userContract.getUserScore(staker);
+                        uint256 userStake = stakers[currentMemeToken][staker]
+                            .amount;
+                        uint256 userScoreBefore = userContract.getUserScore(
+                            staker
+                        );
 
                         if (isWinningMeme) {
                             // Calculate reward proportional to stake
-                            uint256 reward = (userStake * totalStake) / winningStake;
+                            uint256 reward = (userStake * totalStake) /
+                                winningStake;
                             payable(staker).transfer(reward);
-                            emit RewardDistributed(staker, currentMemeToken, reward);
+                            emit RewardDistributed(
+                                staker,
+                                currentMemeToken,
+                                reward
+                            );
                             userContract.updateWinStats(staker);
                         } else {
                             userContract.updateLossStats(staker);
                         }
 
-                        uint256 userScoreAfter = userContract.getUserScore(staker);
-                        emit UserScoreUpdated(staker, userScoreBefore, userScoreAfter);
+                        uint256 userScoreAfter = userContract.getUserScore(
+                            staker
+                        );
+                        emit UserScoreUpdated(
+                            staker,
+                            userScoreBefore,
+                            userScoreAfter
+                        );
 
                         // Clear staker's data
                         delete stakers[currentMemeToken][staker];
                     }
                 }
 
-            // Reset total staked amount for this meme
-            totalStakedPerMeme[currentMemeToken] = 0;
+                // Reset total staked amount for this meme
+                totalStakedPerMeme[currentMemeToken] = 0;
             }
 
-        // Ensure all funds have been distributed
-        require(address(this).balance == 0, "Not all funds distributed");
+            // Ensure all funds have been distributed
+            require(address(this).balance == 0, "Not all funds distributed");
         }
     }
 
@@ -126,44 +174,41 @@ contract MemeBattle is ISPHook {
         uint64 schemaId,
         uint64 attestationId,
         bytes calldata extraData
-    ) external payable  {
+    ) external payable {
         // Implement revocation logic if needed
     }
 
     function didReceiveAttestation(
-            address attester,
-            uint64, // schemaId
-            uint64, // attestationId
-            IERC20, // resolverFeeERC20Token
-            uint256, // resolverFeeERC20Amount
-            bytes calldata // extraData
-        )
-            external
-            view
-        {
+        address attester,
+        uint64, // schemaId
+        uint64, // attestationId
+        IERC20, // resolverFeeERC20Token
+        uint256, // resolverFeeERC20Amount
+        bytes calldata // extraData
+    ) external view {
         // Logic here
-        }
+    }
 
     function didReceiveRevocation(
-            address attester,
-            uint64, // schemaId
-            uint64, // attestationId
-            IERC20, // resolverFeeERC20Token
-            uint256, // resolverFeeERC20Amount
-            bytes calldata // extraData
-        )
-            external
-            view
-        {
+        address attester,
+        uint64, // schemaId
+        uint64, // attestationId
+        IERC20, // resolverFeeERC20Token
+        uint256, // resolverFeeERC20Amount
+        bytes calldata // extraData
+    ) external view {
         // Logic here
-        }
-
-    
-
+    }
 
     function unstake(address memeToken) external {
-        require(stakers[memeToken][msg.sender].hasStaked, "No staked tokens found");
-        require(stakers[memeToken][msg.sender].amount > 0, "No staked tokens to unstake");
+        require(
+            stakers[memeToken][msg.sender].hasStaked,
+            "No staked tokens found"
+        );
+        require(
+            stakers[memeToken][msg.sender].amount > 0,
+            "No staked tokens to unstake"
+        );
 
         uint256 amount = stakers[memeToken][msg.sender].amount;
         payable(msg.sender).transfer(amount);
@@ -179,7 +224,10 @@ contract MemeBattle is ISPHook {
 
     function endBattle(address _winningMemeToken) external {
         require(msg.sender == creator, "Only the creator can end the battle");
-        require(validMemeToken(_winningMemeToken), "Invalid winning meme token");
+        require(
+            validMemeToken(_winningMemeToken),
+            "Invalid winning meme token"
+        );
         require(!battleEnded, "Battle already ended");
 
         winningMemeToken = _winningMemeToken;
@@ -199,34 +247,54 @@ contract MemeBattle is ISPHook {
             }
         }
 
-        uint256 rewardPool = totalStakedPerMeme[winningMemeToken] + totalLosingStake;
+        uint256 rewardPool = totalStakedPerMeme[winningMemeToken] +
+            totalLosingStake;
 
         for (uint256 i = 0; i < memeTokenAddresses.length; i++) {
             if (memeTokenAddresses[i] == winningMemeToken) {
-                for (uint256 j = 0; j < memeTokenAddresses.length; j++) {
-                    address staker = address(uint160(j)); // This is a placeholder, replace with actual staker address
+                for (
+                    uint256 j = 0;
+                    j < stakersPerMeme[memeTokenAddresses[i]].length;
+                    j++
+                ) {
+                    address staker = stakersPerMeme[memeTokenAddresses[i]][j];
                     if (stakers[memeTokenAddresses[i]][staker].amount > 0) {
-                        uint256 userStake = stakers[memeTokenAddresses[i]][staker].amount;
-                        uint256 reward = (userStake * rewardPool) / totalStakedPerMeme[winningMemeToken];
+                        uint256 userStake = stakers[memeTokenAddresses[i]][
+                            staker
+                        ].amount;
+                        uint256 reward = (userStake * rewardPool) /
+                            totalStakedPerMeme[winningMemeToken];
                         payable(staker).transfer(reward);
-                        emit RewardDistributed(staker, memeTokenAddresses[i], reward);
-                        
+                        emit RewardDistributed(
+                            staker,
+                            memeTokenAddresses[i],
+                            reward
+                        );
+
                         // Update user win stats
                         userContract.updateWinStats(staker);
                     }
                 }
             } else {
-                for (uint256 j = 0; j < memeTokenAddresses.length; j++) {
-                    address staker = address(uint160(j)); // This is a placeholder, replace with actual staker address
+                for (
+                    uint256 j = 0;
+                    j < stakersPerMeme[memeTokenAddresses[i]].length;
+                    j++
+                ) {
+                    address staker = stakersPerMeme[memeTokenAddresses[i]][j];
                     if (stakers[memeTokenAddresses[i]][staker].amount > 0) {
                         // Update user loss stats
                         userContract.updateLossStats(staker);
                     }
                 }
-                
+
                 // Handle losing tokens (e.g., burn or redistribute)
-                uint256 burnAmount = (totalStakedPerMeme[memeTokenAddresses[i]] * 20) / 100;
-                uint256 transferAmount = totalStakedPerMeme[memeTokenAddresses[i]] - burnAmount;
+                uint256 burnAmount = (totalStakedPerMeme[
+                    memeTokenAddresses[i]
+                ] * 20) / 100;
+                uint256 transferAmount = totalStakedPerMeme[
+                    memeTokenAddresses[i]
+                ] - burnAmount;
                 payable(address(this)).transfer(burnAmount);
                 payable(address(winningMemeToken)).transfer(transferAmount);
             }
@@ -246,7 +314,9 @@ contract MemeBattle is ISPHook {
         return userContract.getUserScore(user);
     }
 
-    function getUserStats(address user) external view returns (User.UserStats memory) {
+    function getUserStats(
+        address user
+    ) external view returns (User.UserStats memory) {
         return userContract.getUserStats(user);
     }
 
